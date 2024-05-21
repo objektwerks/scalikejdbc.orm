58588801202d7f962f9edf154020316c86eaadb5
objektwerks <objektwerks@runbox.com>
2024-05-21 14:02:23 -0400

    edited todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -8,5 +8,5 @@ final case class Todo(id: Long = 0, task: String)
 object Todo extends CRUDMapper[Todo]:
   override val tableName = "todo"
   override val defaultAlias = createAlias("todo")
-  def extract(wrappedResultSet: WrappedResultSet,
-              resultName: ResultName[Todo]): Todo = autoConstruct(wrappedResultSet, resultName)
+  override def extract(wrappedResultSet: WrappedResultSet,
+                       resultName: ResultName[Todo]): Todo = autoConstruct(wrappedResultSet, resultName)
