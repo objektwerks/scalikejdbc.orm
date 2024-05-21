aa0cfa33497a0a8842809eecdd49cb5a5562f216
objektwerks <objektwerks@runbox.com>
2024-05-21 07:05:51 -0400

    enhanced todo

--- src/main/scala/objektwerks/Todo.scala
+++ src/main/scala/objektwerks/Todo.scala
@@ -1,3 +1,12 @@
 package objektwerks
 
+import scalikejdbc.{autoConstruct, ResultName, WrappedResultSet}
+import scalikejdbc.orm.CRUDMapper
+
 final case class Todo(id: Int = 0, task: String)
+
+object Todo extends CRUDMapper[Todo]:
+  override val tableName = "todo"
+  override val defaultAlias = createAlias("todo")
+  def extract(wrappedResultSet: WrappedResultSet,
+              resultName: ResultName[Todo]): Todo = autoConstruct(wrappedResultSet, resultName)
